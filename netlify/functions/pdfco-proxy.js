601099219d3503c30b6deaeebca3a34dfdd6d29f
Aadelalsaadi <alsaadi.legend@gmail.com>
2026-05-02 22:41:51 +0300

    Update pdfco-proxy.js

--- netlify/functions/pdfco-proxy.js
+++ netlify/functions/pdfco-proxy.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const https = require('https');
 
 exports.handler = async (event) => {
   if (event.httpMethod === 'OPTIONS') {
@@ -14,16 +14,31 @@ exports.handler = async (event) => {
   }
 
   try {
-    const { endpoint, body } = JSON.parse(event.body);
     const API_KEY = 'alsaadi.legend@gmail.com_7FtrjwweCnnIMe5Kxo8hkWeFREJzYGaHjQK4C7a3OkR2XaK7daD3DVgozSoKAtyj';
+    const { endpoint, body } = JSON.parse(event.body);
 
-    const response = await fetch(`https://api.pdf.co/v1/${endpoint}`, {
-      method: 'POST',
-      headers: { 'x-api-key': API_KEY, 'Content-Type': 'application/json' },
-      body: JSON.stringify(body)
+    const requestBody = JSON.stringify(body);
+
+    const data = await new Promise((resolve, reject) => {
+      const req = https.request({
+        hostname: 'api.pdf.co',
+        path: `/v1/${endpoint}`,
+        method: 'POST',
+        headers: {
+          'x-api-key': API_KEY,
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(requestBody)
+        }
+      }, (res) => {
+        let responseBody = '';
+        res.on('data', chunk => responseBody += chunk);
+        res.on('end', () => resolve(JSON.parse(responseBody)));
+      });
+      req.on('error', reject);
+      req.write(requestBody);
+      req.end();
     });
 
-    const data = await response.json();
     return {
       statusCode: 200,
       headers: { 'Access-Control-Allow-Origin': '*' },
